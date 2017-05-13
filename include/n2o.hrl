ca0ed12ed2c1de81f6422676d2eb657e7ac50179
Renaud Tripathi <ganeshrd@laposte.net>
2017-05-14 00:21:20 +0200

    CTX#cx definition

--- include/n2o.hrl
+++ include/n2o.hrl
@@ -1,3 +1,3 @@
 -record(ev, { module, msg, trigger, name }).
--record(cx, { handlers, actions, req, module, lang, path, session, params, form, state=[] }).
+-record(cx, { handlers, actions, req, module, lang, path, session, formatter=false, params, form, state=[] }).
 -define(CTX, (get(context))).
